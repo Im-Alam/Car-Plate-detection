50a6e8822cc83d30e73e340258a802ef1df1e1f3
Im-Alam <imranalam04418@gmail.com>
2024-05-28 20:22:33 +0530

    csv file write metod updated

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template,jsonify, Response, request
 from number_plate_recognition import recognize_plate
 import cv2
+import csv
 import pandas as pd
+import os
+import signal
+from time import sleep
 
-cars = pd.read_csv('tfl (1)/tfl/uploads/cars.csv')
+
+file_path_ = 'tfl (1)/tfl/uploads/cars.csv'
 
 
 app = Flask(__name__)
@@ -12,14 +17,20 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-def generate_frames():
+def generate_frames(file_path = file_path_):
     cap = cv2.VideoCapture(0)
     cap.set(3, 640) # width of camera frame
-    cap.set(4, 480) # height
+    cap.set(4, 400) # height
     
     if not cap.isOpened():
         print("Error: Could not open camera.")
         return
+
+    with open(file_path, mode='a', newline='') as csvfile:
+        # Create a DictWriter object, specifying the fieldnames
+        fieldnames = ['Timestamp', 'CarNumber']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
 
     while True:
         ret, frame = cap.read()
@@ -27,18 +38,44 @@ def generate_frames():
             print("Error: Could not read frame.")
             break
 
-        frame_with_plate = recognize_plate(frame)  # Assuming recognize_plate now takes a frame as input
+        frame_with_plate, c_time, car_number = recognize_plate(frame)
 
-        _, buffer = cv2.imencode('.jpg', frame_with_plate)
-        frame_with_plate = buffer.tobytes()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame_with_plate + b'\r\n\r\n')
+        if frame_with_plate is not None:
+            _, buffer = cv2.imencode('.jpg', frame_with_plate)
+            frame_with_plate = buffer.tobytes()
+            yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame_with_plate + b'\r\n\r\n')
 
+        if car_number is not None:
+            # Write data to CSV only if car number is recognized
+            data = {'Timestamp': c_time, 'CarNumber': car_number}
+            writer.writerow(data)
+    
     cap.release()
 
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    if request.method == 'POST':
+        shutdown_server()
+        return 'Server shutting down...'
+    else:
+        return 'Invalid request method.'
+
+def shutdown_server():
+    pid = os.getpid()
+    os.kill(pid, signal.SIGINT)
+
+@app.route('/get_csv_data')
+def get_csv_data():
+    # Load the CSV file into a DataFrame
+    df = pd.read_csv('tfl (1)/tfl/uploads/cars.csv')
+    # Convert DataFrame to JSON
+    data = df.to_dict(orient='records')
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- number_plate_recognition.py
+++ number_plate_recognition.py
@@ -1,22 +1,22 @@
 import cv2
-import pytesseract
-import time
-
-pytesseract.pytesseract.tesseract_cmd = 'tesseract'
+from openalpr import Alpr
+from datetime import datetime
 
 
 def recognize_plate(frame):
-    harcascade = 'haarcascade_russian_plate_number.xml'
+    harcascade = 'tfl (1)/tfl/haarcascade_russian_plate_number.xml'
     plate_cascade = cv2.CascadeClassifier(harcascade)
+    alpr = Alpr("us", "/etc/openalpr/openalpr.conf", "/usr/share/openalpr/runtime_data")
 
     img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+    c_time = None
+    outputT = None
     # find the coordinate for plates in the image
     plates = plate_cascade.detectMultiScale(img_gray, 1.1, 4)  # Adjust parameters as needed
 
     for (x, y, w, h) in plates:
         area = w * h
-        min_area = 100
+        min_area = 700
         if area > min_area:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 220, 0), 1)
             cv2.putText(frame, 'Number Plate', (x, y - 5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 0), 1)
@@ -24,15 +24,17 @@ def recognize_plate(frame):
             img_roi = frame[y:y+h, x:x+w]  # Corrected the variable name
             
             try:
-                # Use Tesseract for OCR
+                # Use easy for OCR
                 custom_config = r'--oem 3 --psm 1'  # Adjust OCR parameters as needed
+                outputT = None
+                outputT = alpr.recognize(img_roi)
+                cv2.putText(frame, outputT, (x, y - 20), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 0, 0), 1)
+                
+                c_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                
 
-                output = pytesseract.image_to_string(img_roi, config=custom_config)
-                cv2.putText(frame, output, (x, y - 20), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 0, 0), 1)
-                
-                t = time.localtime()
-                current_time = time.strftime("%H:%M:%S", t)
-                
             except Exception as e:
-                print(f"Error in OCR: {e}")
-    return frame
+                print(f"Error in OCR: {e} ++++++++++++++++++++++++++++++")
+    return frame, c_time, outputT
+
+ 
